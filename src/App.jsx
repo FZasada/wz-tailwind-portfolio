7e544d3833b396d66da61a2cef87180ace218895
FlorianZasada <florian.zasada@gmail.com>
2025-04-23 00:06:06 +0200

    Fixed scrolling issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,37 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import { useEffect, useRef, useState, useLayoutEffect } from 'react';
 
 import Home from './screens/Home';
 import Project from './screens/Project';
 import PageNotFound from './screens/404';
-
-import Navbar from "./components/Navbar"
-import Footer from './components/Footer';
-import Topbutton from './components/elements/Topbutton';
-
-import Contact from './components/Contact';
-
-
-import { useEffect, useRef, useState } from 'react';
 import MyProjects from './screens/MyProjects';
 import Aboutme from './screens/Aboutme';
 
+import Navbar from "./components/Navbar";
+import Footer from './components/Footer';
+import Topbutton from './components/elements/Topbutton';
+import Contact from './components/Contact';
+
+const Wrapper = ({ children }) => {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+  }, [location.pathname]);
+
+  return children;
+};
 
 const App = () => {
   const [showTopUp, setShowTopUp] = useState(false);
   const refScrollUp = useRef();
 
   const handleVisibleTopUp = () => {
-    if (window.scrollY > 100) {
-      setShowTopUp(true);
-    } else {
-      setShowTopUp(false);
-    }
+    setShowTopUp(window.scrollY > 100);
   };
 
   const handleScrollUp = () => {
-    if (refScrollUp.current) {
-      refScrollUp.current.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
+    refScrollUp.current?.scrollIntoView({ behavior: "smooth" });
   };
 
   useEffect(() => {
@@ -42,28 +40,27 @@ const App = () => {
   }, []);
 
   return (
-    <div className='overflow-x-hidden text-neutral-900 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
-      <Router>
-        <div className='container mx-auto px-6 lg:px-20 pt-[80px] md:pt-0' ref={refScrollUp}>
-        <Navbar />
-          <Topbutton
-            showTopUp={showTopUp}
-            scrollUp={handleScrollUp}
-          />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/project/:id" element={<Project />} />
-            <Route path="/portfolio" element={<MyProjects />} />
-            <Route path="/aboutme" element={<Aboutme />} />
-            <Route path="/404" element={<PageNotFound />} />
-            <Route path="*" element={<Navigate to={"/404"} replace />} />
-          </Routes>
-          <Contact />
-          <Footer />
-        </div>
-      </Router>
-  </div>
-  )
-}
+      <div className="overflow-x-hidden text-neutral-900 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+        <Router>
+          <Wrapper>
+            <Navbar />
+            <div className="container mx-auto px-6 lg:px-20 pt-[80px] md:pt-0" ref={refScrollUp}>
+              <Topbutton showTopUp={showTopUp} scrollUp={handleScrollUp} />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/project/:id" element={<Project />} />
+                <Route path="/portfolio" element={<MyProjects />} />
+                <Route path="/aboutme" element={<Aboutme />} />
+                <Route path="/404" element={<PageNotFound />} />
+                <Route path="*" element={<Navigate to="/404" replace />} />
+              </Routes>
+              <Contact />
+              <Footer />
+            </div>
+          </Wrapper>
+        </Router>
+      </div>
+  );
+};
 
-export default App
+export default App;
