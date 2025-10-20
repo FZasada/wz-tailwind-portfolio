3d366694281a4b5d8f69d46de174d8f697711fa6
FlorianZasada <florian.zasada@gmail.com>
2025-10-20 20:59:36 +0200

    Added animation

--- src/components/pages/home/Projects.jsx
+++ src/components/pages/home/Projects.jsx
@@ -6,31 +6,22 @@ import ProjectCard from "../../elements/ProjectCard"
 const Projects = ({ title, projects }) => {
     return (
         <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
+            initial={{ y: 20, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
             transition={{
                 ease: "easeInOut",
                 duration: 0.6,
             }}
-            viewport={{ once: true, amount: 0.3 }}
+        viewport={{ once: true, amount: 0.5 }}
             className='flex flex-col pb-4 items-center'>
             {title && <h2
                 className='my-8 text-center text-[48px] font-bold'>{ title }</h2>
             }
             <div className="flex lg:flex-row flex-col gap-[48px]">
                 {projects.map((project, index) => (
-                    <motion.div
-                        initial={{ y: 0, opacity: 0 }}
-                        whileInView={{ y: 0, opacity: 1 }}
-                        transition={{
-                            ease: "easeInOut",
-                            duration: 0.6,
-                            delay: index * 0.1
-                        }}
-                        viewport={{ once: true }}
-                        key={index}>
+                    <div key={index}>
                         <ProjectCard project={project} />
-                    </motion.div>
+                    </div>
                 ))}
             </div>
         </motion.div>
